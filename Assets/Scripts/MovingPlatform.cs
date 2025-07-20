35fedc7b4eeee5a2df3c5d5287f0f78abe58befd
Yana <77960186+yanakrupets@users.noreply.github.com>
2025-07-20 12:26:52 +0300

    moving platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private float speed;
+    
+    private Vector3 _nextPosition;
+    private const float DistanceThreshold = 0.1f;
+
+    private void Start()
+    {
+        _nextPosition = pointB.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _nextPosition, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, _nextPosition) <= DistanceThreshold)
+        {
+            _nextPosition = _nextPosition == pointA.position ? pointB.position : pointA.position;
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
