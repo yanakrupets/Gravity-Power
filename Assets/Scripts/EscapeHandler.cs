15329c15030ba3ff61c56a416ac7cd2f01cede7c
Yana <77960186+yanakrupets@users.noreply.github.com>
2025-08-14 16:54:35 +0300

    escape handler
    when press esc you go back to menu (start)

---
+++ Assets/Scripts/EscapeHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EscapeHandler : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset inputActionAsset;
+    
+    private InputAction _openMenuAction;
+
+    private void OnEnable()
+    {
+        _openMenuAction = inputActionAsset.FindAction("MenuOpen");
+        
+        _openMenuAction.performed += LoadStart;
+        
+        _openMenuAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _openMenuAction.performed -= LoadStart;
+        
+        _openMenuAction.Disable();
+    }
+    
+    private void LoadStart(InputAction.CallbackContext context)
+    {
+        LevelController.LoadStart();
+    }
+}
